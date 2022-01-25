e56fab0673c22f7ddf789740cb933d49d2100049
Vivek Maskara <maskaravivek@gmail.com>
2022-01-25 11:52:29 +0530

    Bug fixes

--- app/src/main/java/com/example/androidmasterclass/activity/module2/Lesson2ExampleActivity.kt
+++ app/src/main/java/com/example/androidmasterclass/activity/module2/Lesson2ExampleActivity.kt
@@ -2,22 +2,25 @@ package com.example.androidmasterclass.activity.module2
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.widget.AppCompatTextView
+import android.widget.TextView
 import com.example.androidmasterclass.R
 
 class Lesson2ExampleActivity : Activity() {
-    private lateinit var tvCounter: AppCompatTextView
     private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_module_2_lesson_2)
-        tvCounter = findViewById(R.id.tv_counter)
     }
 
     override fun onResume() {
         super.onResume()
         count++
-        tvCounter.setText(count)
+        updateCounter()
+    }
+
+    private fun updateCounter() {
+        val tvCounter = findViewById(R.id.tv_counter) as TextView
+        tvCounter.setText(count.toString())
     }
 
 }
