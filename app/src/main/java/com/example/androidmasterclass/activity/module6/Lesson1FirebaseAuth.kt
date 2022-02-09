bc67404dc1f50c8e26e8d6d38615e29d5e4b3699
Vivek Maskara <maskaravivek@gmail.com>
2022-02-09 11:57:10 -0700

    Firebase hosted UI for authentication

--- app/src/main/java/com/example/androidmasterclass/activity/module6/Lesson1FirebaseAuth.kt
+++ app/src/main/java/com/example/androidmasterclass/activity/module6/Lesson1FirebaseAuth.kt
@@ -1,28 +1,37 @@
 package com.example.androidmasterclass.activity.module6
 
+import android.R.attr.data
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidmasterclass.R
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.drawee.view.SimpleDraweeView
-import java.net.URL
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Lesson1FirebaseAuth : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fresco.initialize(this)
         setContentView(R.layout.activity_module_6_lesson_1_firebase_auth)
         startEmailPasswordAuthForSignUp()
         startEmailPasswordAuthForLogin()
+        startFirebaseUIActivity()
     }
 
     private fun startEmailPasswordAuthForSignUp() {
-        val button = findViewById(R.id.email_password_auth_signup) as Button
+        val button = findViewById<Button>(R.id.email_password_auth_signup)
         button.setOnClickListener {
             val intent = Intent(this, EmailPasswordSignupActivity::class.java)
             intent.putExtra("action", "signup")
@@ -31,11 +40,47 @@ class Lesson1FirebaseAuth : AppCompatActivity() {
     }
 
     private fun startEmailPasswordAuthForLogin() {
-        val button = findViewById(R.id.email_password_auth_login) as Button
+        val button = findViewById<Button>(R.id.email_password_auth_login)
         button.setOnClickListener {
             val intent = Intent(this, EmailPasswordSignupActivity::class.java)
             intent.putExtra("action", "login")
             startActivity(intent)
+        }
+    }
+
+    private fun startFirebaseUIActivity() {
+        val button = findViewById<Button>(R.id.firebase_ui_login)
+        button.setOnClickListener {
+            createSignInIntent()
+        }
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(
+                this, "Logged in as ${user?.email}",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            Toast.makeText(
+                this, "Login failed",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
