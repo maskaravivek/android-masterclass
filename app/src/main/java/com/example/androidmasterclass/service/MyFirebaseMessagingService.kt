b37d12ed41af3d5a93455cbd894baff2b02f9712
Vivek Maskara <maskaravivek@gmail.com>
2022-02-11 20:07:31 -0700

    With notification service

---
+++ app/src/main/java/com/example/androidmasterclass/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,34 @@
+package com.example.androidmasterclass.service
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            handleNow()
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+    }
+
+    private fun handleNow() {
+        Log.d(TAG, "Short lived task is done.")
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
